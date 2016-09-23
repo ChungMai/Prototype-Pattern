a6d88718868763bc74088e1b22ece90b62d363f1
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-09-23 16:14:23 +0700

    Done

---
+++ PrototypePattern.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class AbstractCharacter{
+    var  name : String?
+    var mana : Int?
+    var attack : Int?
+    var defense : Int?
+    
+    init(name:String?, mana:Int?, attack:Int?, defense:Int?){
+        self.name = name
+        self.mana = mana
+        self.attack = attack
+        self.defense = defense
+    }
+    
+    func clone() -> AbstractCharacter{
+        return AbstractCharacter(name:self.name, mana:self.mana, attack:self.attack, defense:self.defense)
+    }
+}
+
+class Character : AbstractCharacter{
+    override init(name:String?, mana:Int?, attack:Int?, defense:Int?){
+        super.init(name: name, mana: mana, attack: attack, defense: defense)
+    }
+}
+
+
+// Simulate our client
+// This is the card that we will copy
+let raidLeader = Character(name: "Raid Leader", mana: 3, attack: 2, defense: 2)
+// Now we use our faceless Manipulator card to clone the raidleader
+let facelessManipulator = raidLeader.clone()
+print("\(facelessManipulator.name!, facelessManipulator.mana!,facelessManipulator.attack!, facelessManipulator.defense!)")
